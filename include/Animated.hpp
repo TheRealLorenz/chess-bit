2cb168922f6d8c488c6ab94dc4021e317f51cc7f
TheRealLorenz <lollobellina@gmail.com>
2024-06-09 21:50:30 +0200

    Add scaling animation

--- include/Animated.hpp
+++ include/Animated.hpp
@@ -10,11 +10,17 @@ public:
     void setPosition(const sf::Vector2f& position);
     void setPositionAnimated(const sf::Vector2f& position, int pxPerSecond);
 
+    void setScale(const sf::Vector2f& factors);
+    void setScaleAnimated(const sf::Vector2f& factors, int factorPerSecond);
+
     void update(int deltaMillis);
 
 private:
     void updatePosition(int deltaMillis);
+    void updateScale(int deltaMillis);
 
     sf::Vector2f targetPosition;
+    sf::Vector2f targetScale;
     int speedPerSecond;
+    int scaleFactorPerSecond;
 };
--- src/Animated.cpp
+++ src/Animated.cpp
@@ -13,7 +13,7 @@ void Animated::setPosition(const sf::Vector2f& position) {
 }
 
 void Animated::setPositionAnimated(const sf::Vector2f& position,
-                                     int pxPerSecond) {
+                                   int pxPerSecond) {
     targetPosition = position;
     speedPerSecond = pxPerSecond;
 }
@@ -52,6 +52,52 @@ void Animated::updatePosition(int deltaMillis) {
     sf::Transformable::setPosition(position);
 }
 
+void Animated::setScale(const sf::Vector2f& factors) {
+    targetScale = factors;
+    sf::Transformable::setScale(factors);
+}
+
+void Animated::setScaleAnimated(const sf::Vector2f& factors,
+                                int factorPerSecond) {
+    targetScale = factors;
+    scaleFactorPerSecond = factorPerSecond;
+}
+
+void Animated::updateScale(int deltaMillis) {
+    auto scale = getScale();
+    if (scale == targetScale) {
+        return;
+    }
+
+    sf::Vector2f delta = targetScale - scale;
+    float modulo = std::sqrt(delta.x * delta.x + delta.y * delta.y);
+    delta.x /= modulo;
+    delta.y /= modulo;
+
+    if (delta.x > 0) {
+        scale.x = std::min(scale.x + (float)scaleFactorPerSecond * delta.x *
+                                         deltaMillis / 1000,
+                           targetScale.x);
+    } else if (delta.x < 0) {
+        scale.x = std::max(scale.x + (float)scaleFactorPerSecond * delta.x *
+                                         deltaMillis / 1000,
+                           targetScale.x);
+    }
+
+    if (delta.y > 0) {
+        scale.y = std::min(scale.y + (float)scaleFactorPerSecond * delta.y *
+                                         deltaMillis / 1000,
+                           targetScale.y);
+    } else if (delta.y < 0) {
+        scale.y = std::max(scale.y + (float)scaleFactorPerSecond * delta.y *
+                                         deltaMillis / 1000,
+                           targetScale.y);
+    }
+
+    sf::Transformable::setScale(scale);
+}
+
 void Animated::update(int deltaMillis) {
     updatePosition(deltaMillis);
+    updateScale(deltaMillis);
 }
