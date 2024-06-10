628c9001cc7f13c4735d0672a68a2e9a984d685a
TheRealLorenz <lollobellina@gmail.com>
2024-06-10 10:28:49 +0200

    Make Cell not a readonly record

--- include/Cell.hpp
+++ include/Cell.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 struct Cell {
-    const int row;
-    const int column;
+    int row;
+    int column;
 
     bool operator==(const Cell& other) const {
         return row == other.row && column == other.column;
